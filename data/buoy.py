844d81287c6059c0bb950ba605d992a08016cd2e
surfstation <2008147+surfstation@users.noreply.github.com>
2022-06-24 09:21:52 -0400

    update buoy data source from cdip rss to ndbc rss

--- data/buoy.py
+++ data/buoy.py
@@ -7,17 +7,16 @@ import sys
 import datetime
 from dateutil import tz
 
-# http://cdip.ucsd.edu/m/products/?stn=194p1
-# https://cdip.ucsd.edu/rss/194.xml
+# https://www.ndbc.noaa.gov/station_page.php?station=41117
+# https://www.ndbc.noaa.gov/data/latest_obs/41117.rss
 
 result = {'buoy': {}}
 
-
 headers = {
-  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0',
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
 }
 
-r = requests.get('https://cdip.ucsd.edu/rss/194.xml', headers=headers, timeout=60, verify=True)
+r = requests.get('https://www.ndbc.noaa.gov/data/latest_obs/41117.rss', headers=headers, timeout=60, verify=True)
 
 print(r.url)
 
@@ -27,11 +26,11 @@ now = datetime.datetime.now().replace(microsecond=0).isoformat()
 if r.status_code == 200:
   root = ET.fromstring(r.content)
   data = root.findall('./channel/item/description')[0].text
-  result['buoy']['swell_height_feet'] = re.match(r'.*<strong>height</strong>.*\((\d+\.\d+)\s+feet\)', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['swell_period_secs'] = re.match(r'.*<strong>period</strong>\s+(\d+\.\d+)\s+seconds', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['swell_angle_degrees'] = re.match(r'.*<strong>direction</strong>\s+(\d+)\s+degrees', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['temperature_fahrenheit'] = re.match(r'.*<strong>sea\s+surface\s+temp</strong>.*\((\d+\.\d+)\s+F\)', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['when'] = datetime.datetime.strptime(root.findall('./channel/pubDate')[0].text, '%a, %d %b %Y %H:%M:%S %z').astimezone(new_york_time).strftime('%m/%d/%Y %I:%M %p')
+  result['buoy']['swell_height_feet'] = re.match(r'.*<strong>Significant Wave Height:</strong>\s+(\d+\.\d+)\s+ft', data, re.DOTALL | re.IGNORECASE)[1]
+  result['buoy']['swell_period_secs'] = re.match(r'.*<strong>Average Period:</strong>\s+(\d+\.\d+)\s+sec', data, re.DOTALL | re.IGNORECASE)[1]
+  result['buoy']['swell_angle_degrees'] = re.match(r'.*<strong>Mean Wave Direction:</strong>.*\((\d+)&#176;\)', data, re.DOTALL | re.IGNORECASE)[1]
+  result['buoy']['temperature_fahrenheit'] = re.match(r'.*<strong>Water Temperature:</strong>\s+(\d+\.\d+)&#176;F\s+', data, re.DOTALL | re.IGNORECASE)[1]
+  result['buoy']['when'] = datetime.datetime.strptime(root.findall('./channel/item/pubDate')[0].text, '%a, %d %b %Y %H:%M:%S %z').astimezone(new_york_time).strftime('%m/%d/%Y %I:%M %p')
   result['buoy']['timestamp'] = now
   with open(sys.argv[1], 'w') as f:
     f.write(json.dumps(result, sort_keys=False, indent=2))
