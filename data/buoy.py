6fbec9eee6cceee3cbc25ea1d16e7bba99d0a35f
surfstation <2008147+surfstation@users.noreply.github.com>
2023-01-20 03:50:43 -0500

    be more resilient when the data feed does not contain all data items

--- data/buoy.py
+++ data/buoy.py
@@ -26,10 +26,18 @@ now = datetime.datetime.now().replace(microsecond=0).isoformat()
 if r.status_code == 200:
   root = ET.fromstring(r.content)
   data = root.findall('./channel/item/description')[0].text
-  result['buoy']['swell_height_feet'] = re.match(r'.*<strong>Significant Wave Height:</strong>\s+(\d+\.\d+)\s+ft', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['swell_period_secs'] = re.match(r'.*<strong>Average Period:</strong>\s+(\d+\.\d+)\s+sec', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['swell_angle_degrees'] = re.match(r'.*<strong>Mean Wave Direction:</strong>.*\((\d+)&#176;\)', data, re.DOTALL | re.IGNORECASE)[1]
-  result['buoy']['temperature_fahrenheit'] = re.match(r'.*<strong>Water Temperature:</strong>\s+(\d+\.\d+)&#176;F\s+', data, re.DOTALL | re.IGNORECASE)[1]
+  match = re.match(r'.*<strong>Significant Wave Height:</strong>\s+(\d+\.\d+)\s+ft', data, re.DOTALL | re.IGNORECASE)
+  if match:
+    result['buoy']['swell_height_feet'] = match[1]
+  match = re.match(r'.*<strong>Average Period:</strong>\s+(\d+\.\d+)\s+sec', data, re.DOTALL | re.IGNORECASE)
+  if match:
+    result['buoy']['swell_period_secs'] = match[1]
+  match = re.match(r'.*<strong>Mean Wave Direction:</strong>.*\((\d+)&#176;\)', data, re.DOTALL | re.IGNORECASE)
+  if match:
+    result['buoy']['swell_angle_degrees'] = match[1]
+  match = re.match(r'.*<strong>Water Temperature:</strong>\s+(\d+\.\d+)&#176;F\s+', data, re.DOTALL | re.IGNORECASE)
+  if match:
+    result['buoy']['temperature_fahrenheit'] = match[1]
   result['buoy']['when'] = datetime.datetime.strptime(root.findall('./channel/item/pubDate')[0].text, '%a, %d %b %Y %H:%M:%S %z').astimezone(new_york_time).strftime('%m/%d/%Y %I:%M %p')
   result['buoy']['timestamp'] = now
   with open(sys.argv[1], 'w') as f:
